dd6712d33d2a767b071d2085547dac85b9fece70
Elena Petrova <elena.i.11@yandex.ru>
2023-07-18 22:13:48 +0400

    git remote add origin git@github.com:ElenaPetrova250/Pythonpro.git git branch -M main git push -u origin main

---
+++ sem8task5.py
@@ -0,0 +1,24 @@
+import os
+import pickle
+import csv
+import json
+
+
+# Задача 5
+# Напишите функцию, которая ищет json файлы
+# в указанной директории и сохраняет
+# их содержимое в виде одноимённых pickle файлов.
+
+
+def search_files(ext: str = '.json', dir_: str = '.') -> None:
+
+    files = (file for file in os.listdir(dir_) if file.endswith(ext))
+
+    for file in files:
+        name, _ = os.path.splitext(file)
+        with (
+            open(file, 'r') as r_file,
+            open(name + '.pickle', 'wb') as w_file
+        ):
+            data = r_file.read()
+            pickle.dump(file=w_file, obj=data)
